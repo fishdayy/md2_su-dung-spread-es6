0038c0c5d683c3f895f85f38f64b0f26202928ab
Tuấn Anh <110382884+fishdayy@users.noreply.github.com>
2022-09-06 10:49:59 +0700

    bookLish

--- Typescript/qlttStaff.js
+++ Typescript/qlttStaff.js
@@ -4,8 +4,8 @@ var Gender;
     Gender[Gender["Female"] = 1] = "Female";
     Gender[Gender["Orther"] = 2] = "Orther";
 })(Gender || (Gender = {}));
-var Employee = /** @class */ (function () {
-    function Employee(name, gender, birthday, email, phone) {
+class Employee {
+    constructor(name, gender, birthday, email, phone) {
         this.name = "";
         this.gender = Gender.Orther;
         this.email = "";
@@ -16,9 +16,8 @@ var Employee = /** @class */ (function () {
         this.email = email;
         this.phone = phone;
     }
-    return Employee;
-}());
-var employeeList = [];
+}
+let employeeList = [];
 employeeList.push(new Employee("Nguyen Van A", Gender.Male, new Date("1990-09-19"), "anguyen@gmail.com", "0123456789"));
 employeeList.push(new Employee("Tran Thi A", Gender.Female, new Date("1991-10-12"), "btran@gmail.com", "0123456790"));
 employeeList.push(new Employee("Huynh An Nhien", Gender.Orther, new Date("1990-09-19"), "nhienhuynh@gmail.com", "0123456791"));
@@ -26,3 +25,4 @@ function showEmployee(employee) {
     console.log(employee);
 }
 employeeList.forEach(showEmployee);
+//# sourceMappingURL=qlttStaff.js.map
